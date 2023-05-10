4e88d4def5d174709b1a2280480d7476c666922a
Anastasiia Lytus <litusnastja@gmail.com>
2023-05-10 22:11:40 +0300

    first hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesList = document.querySelector('.gallery');
+
+const imagesMarkup = createImagesMarkup(galleryItems);
+
+imagesList.innerHTML = imagesMarkup;
+
+function createImagesMarkup(images) {
+  return images
+    .map(
+      image => `
+<li class="gallery__item">
+   <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+   </a>
+</li>`
+    )
+    .join('');
+}
+
+new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});

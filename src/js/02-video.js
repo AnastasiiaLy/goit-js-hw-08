67fdd6591730e8e6650104a4d9b0ab3e8e7300fb
Anastasiia Lytus <litusnastja@gmail.com>
2023-05-11 18:03:09 +0300

    2nd hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const localKey = 'videoplayer-current-time';
+const showCurrentTime = localStorage.getItem(localKey) || 0;
+
+const onPlay = function (data) {
+  const currentVideoTime = data.seconds;
+  localStorage.setItem(localKey, currentVideoTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(showCurrentTime).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      break;
+    default:
+      break;
+  }
+});
